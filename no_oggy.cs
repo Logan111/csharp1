85def7f7b974adb0cf0075471eeec9afe377f075
BUNARI PRANAY <36372708+Logan111@users.noreply.github.com>
2020-08-13 14:15:07 +0530

    oggy2

--- no_oggy.cs
+++ no_oggy.cs
@@ -1,33 +1,43 @@
+
 using System;
 using System.Collections.Generic;
+
 
 class NoOggyChecker
 {
   static void RemoveOggy(List<string> names)
   {
-    foreach (var name in names.Reverse<string>())
-    {
-        if(name.StartsWith("oggy")) {
-            Console.WriteLine($"Need to remove {name}!");
-            names.Remove(name);
-             //break; you can stop once you have found first oggy , this crash is due to iterator modification
+        for (int i = 0; i < names.Count; i++) 
+        {
+       
+        if (names[i].StartsWith("oggy")) {
+             Console.WriteLine($"Need to remove {names[i]}!");
+             names.RemoveAt(i);  
+            i--;
         }
-    }
-      
+       }
   }
   static bool OggyIsRemoved(List<string> names)
   {
     return names.Find(x => x.Contains("oggy")) == null;
   }
+    
+    
+    
+    
   static int Main()
   {
-    var names = new List<string>
-      { "nobita", "bheem", "oggy", "oggy cockroach", "ninja" };
+        var names = new List<string>{ "nobita", "bheem", "oggy", "oggy cockroach", "ninja" };
+      
     RemoveOggy(names);
-    if(OggyIsRemoved(names)) {
+      
+    if(OggyIsRemoved(names)) 
+    {
       Console.WriteLine("Passed!");
       return 0;
-    } else {
+    } 
+      
+      else {
       Console.WriteLine("Failed!!");
       return 1;
     }
